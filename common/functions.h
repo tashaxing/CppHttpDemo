8ad49e0782324dda40e0f8afad9a987caa631152
Tommy Zhang <3079048471@qq.com>
2023-04-15 20:26:52 +0800

    Add files via upload

---
+++ common/functions.h
@@ -0,0 +1,56 @@
+#pragma once
+
+//----------------------------------------------------------------------
+
+static char* U2G(const char* utf8) {
+	int len = MultiByteToWideChar(CP_UTF8, 0, utf8, -1, NULL, 0);
+	wchar_t* wstr = new wchar_t[len + 1];
+	memset(wstr, 0, len + 1);
+	MultiByteToWideChar(CP_UTF8, 0, utf8, -1, wstr, len);
+	len = WideCharToMultiByte(CP_ACP, 0, wstr, -1, NULL, 0, NULL, NULL);
+	char* str = new char[len + 1];
+	memset(str, 0, len + 1);
+	WideCharToMultiByte(CP_ACP, 0, wstr, -1, str, len, NULL, NULL);
+	if (wstr) delete[] wstr;
+	return str;
+}
+/*
+* blog.csdn.net/weixin_42165585/article/details/81385047
+*/
+//--------------------------------------------------------------------
+//--------------------------------------------------------------------
+
+/*
+* �˴�����CSDN����Դ��
+*ԭ�����ӣ�https ://blog.csdn.net/yozidream/article/details/22789147
+*/
+
+typedef std::uint64_t hash_t;
+
+constexpr hash_t prime = 0x100000001B3ull;
+constexpr hash_t basis = 0xCBF29CE484222325ull;
+
+hash_t hash_(char const* str)
+{
+	hash_t ret{ basis };
+
+	while (*str) {
+		ret ^= *str;
+		ret *= prime;
+		str++;
+	}
+
+	return ret;
+}
+
+constexpr hash_t hash_compile_time(char const* str, hash_t last_value = basis)
+{
+	return *str ? hash_compile_time(str + 1, (*str ^ last_value) * prime) : last_value;
+}
+
+constexpr unsigned long long operator "" _hash(char const* p, size_t)
+{
+	return hash_compile_time(p);
+}
+
+//-----------------------------------------------------------------
---
+++ common/winini.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <cstdlib>
+#include <map>
+
+using namespace std;
+
+//INI文件结点存储结构
+class ININode
+{
+public:
+	ININode(string root, string key, string value)
+	{
+		this->root = root;
+		this->key = key;
+		this->value = value;
+	}
+	string root;
+	string key;
+	string value;
+};
+
+//键值对结构体
+class SubNode
+{
+public:
+	void InsertElement(string key, string value)
+	{
+		sub_node.insert(pair<string, string>(key, value));
+	}
+	map<string, string> sub_node;
+};
+
+//INI文件操作类
+class CMyINI
+{
+public:
+	CMyINI();
+	~CMyINI();
+
+public:
+	int ReadINI(string path);													//读取INI文件
+	string GetValue(string root, string key);									//由根结点和键获取值
+	vector<ININode>::size_type GetSize() { return map_ini.size(); }				//获取INI文件的结点数
+	vector<ININode>::size_type SetValue(string root, string key, string value);	//设置根结点和键获取值
+	int WriteINI(string path);			//写入INI文件
+	void Clear() { map_ini.clear(); }	//清空
+	void Travel();						//遍历打印INI文件
+private:
+	map<string, SubNode> map_ini;		//INI文件内容的存储变量
+};
+
+//http://blog.csdn.net/m_buddy/article/details/54097131
